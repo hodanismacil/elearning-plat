4b818c3de78d51d909eb9097cc86942e70eea05f
Hodan Ismacil <hodanismacil256@gmail.com>
2026-04-20 02:07:55 -0700

    errors buttons

--- src/components/pages/course-page/course.tsx
+++ src/components/pages/course-page/course.tsx
@@ -1,4 +1,4 @@
-import { Navigate } from "react-router-dom";
+
 
 const Course = () => {
   const courses = [
@@ -59,7 +59,7 @@ const Course = () => {
           {courses.map((course) => (
             <div
               key={course.id}
-              // onClick={() => Navigate(`/course/${course.id}`)}
+            
               className="bg-white p-6 rounded-2xl shadow hover:shadow-xl cursor-pointer transition"
             >
               <img
